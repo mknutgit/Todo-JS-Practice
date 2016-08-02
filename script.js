e9558e5d61220a1e877eb1fc564b24c099d648bb
Matt <mknut001@gmail.com>
2016-08-01 23:15:01 -0700

    Add initial files

---
+++ script.js
@@ -0,0 +1,21 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
